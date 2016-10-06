a4e58ccf9340409953efe455b18cce3e1d228693
Oriol Mirosa <oriolmirosa@gmail.com>
2016-10-06 16:15:23 -0500

    Settled on the Johns background for the landing page, and changed the password

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -4,11 +4,12 @@ var session = require('express-session');
 module.exports = {
 	password: function(req, res) {
 		var password = req.body.password;
+		password = password.toLowerCase();
 		var user = "visitor";
-	  	if (!password || password !== 'sando') {
+	  	if (!password || password !== 'partytime') {
 	  		console.log('wrong password');
 	    	res.send('401');
-	  	} else if (password === 'sando') {
+	  	} else if (password === 'partytime') {
 	    	req.session.user = 'visitor';
 	    	req.session.admin = true;
 	    	res.send( { redirect: '/' } );
