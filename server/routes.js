13dfc0b349250fe3d43890d17c7a1f3bcabb7a45
Oriol Mirosa <oriolmirosa@gmail.com>
2016-11-09 19:40:06 -0600

    Ugh. Forgot to re-add the redirect to https

--- server/routes.js
+++ server/routes.js
@@ -8,12 +8,12 @@ var express = require('express'),
 
 module.exports = function(app) {
 
-	// router.get('*',function(req, res, next){
-	//   if(req.headers['x-forwarded-proto']!='https')
-	//     res.redirect('https://TheKarosas.com' + req.url)
-	//   else
-	//     next()
-	// })
+	router.get('*',function(req, res, next){
+	  if(req.headers['x-forwarded-proto']!='https')
+	    res.redirect('https://TheKarosas.com' + req.url)
+	  else
+	    next()
+	})
 
 	// Authentication and Authorization Middleware
 	var auth = function(req, res, next) {
