404a7f2379dd467813277da3b78c4f11f0bf3040
Oriol Mirosa <oriol@mirosa.local>
2017-02-15 08:19:43 -0600

    New try

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -5,15 +5,15 @@ module.exports = {
 	password: function(req, res) {
 		var password = req.body.password;
 		password = password.toLowerCase();
-    var correctPW = process.env.PWD
+    const correctPW = process.env.PWD
 		var user = "visitor";
-	  	if (!password || password !== correctPW) {
-	  		console.log('wrong password');
-	    	res.send('401');
-	  	} else if (password === correctPW) {
-	    	req.session.user = 'visitor';
-	    	req.session.admin = true;
-	    	res.send( { redirect: '/' } );
-	  	}
+  	if (!password || password !== correctPW) {
+  		console.log('wrong password');
+    	res.send('401');
+  	} else if (password === correctPW) {
+    	req.session.user = 'visitor';
+    	req.session.admin = true;
+    	res.send( { redirect: '/' } );
+  	}
 	}
 };
